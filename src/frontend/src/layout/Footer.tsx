b27c5085ab60886c39ee45b42ab5886a84eb38f7
TheY4NN777 <yanisaxel.dabo@aiobi.world>
2026-04-05 15:48:45 +0200

    fix(frontend) remove unused imports in Footer.tsx causing build failure

--- src/frontend/src/layout/Footer.tsx
+++ src/frontend/src/layout/Footer.tsx
@@ -1,64 +1,13 @@
-import { styled } from '@/styled-system/jsx'
 import { css } from '@/styled-system/css'
-import { Link } from '@/primitives'
-import { useTranslation } from 'react-i18next'
-
-const StyledLi = styled('li', {
-  base: {},
-  variants: {
-    divider: {
-      true: {
-        _after: {
-          content: '""',
-          display: 'inline-block',
-          marginX: '.75rem',
-          verticalAlign: 'middle',
-          boxShadow: 'inset 0 0 0 1px #ddd',
-          height: '1rem',
-          width: '1px',
-        },
-      },
-    },
-  },
-})
-
-const InnerContainer = styled('div', {
-  base: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'start',
-    margin: 'auto',
-    maxWidth: '1200px',
-    paddingX: { base: '0.5rem', xs: '1rem', sm: '2rem' },
-  },
-})
-
-const SecondRow = styled('ul', {
-  base: {
-    display: 'flex',
-    borderTop: '1px solid rgb(217 217 217)',
-    paddingTop: '0.5rem',
-    width: '100%',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-  },
-})
-
 
 export const Footer = () => {
-  const { t } = useTranslation('global', { keyPrefix: 'footer' })
-
   return (
     <footer
       className={css({
         borderTop: '2px solid #a251fc',
-        paddingY: '2rem',
+        paddingY: '1rem',
         marginTop: { base: '50px', sm: '100px' },
       })}
-    >
-      <InnerContainer>
-        <SecondRow />
-      </InnerContainer>
-    </footer>
+    />
   )
 }
